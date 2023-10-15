9efcbb228abb766dd211d0501124c0436194d25d
humberto <humberto.lizana@gmail.com>
2023-10-14 23:49:24 -0500

    first commit

---
+++ src/main/java/com/lizana/microservicecreditCard/MicroserviceCreditCardApplication.java
@@ -0,0 +1,13 @@
+package com.lizana.microservicecreditCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCreditCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCreditCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lizana/microservicecreditCard/MicroserviceCreditCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizana.microservicecreditCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCreditCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
